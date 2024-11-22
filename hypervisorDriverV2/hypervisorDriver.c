412ad107d0d8d1cab5deae65321c50154664cd2f
maxfx <maficccc@gmail.com>
2024-11-22 14:51:49 +0100

    hypervisorv2: debug snprintf

--- hypervisorDriverV2/hypervisorDriver.c
+++ hypervisorDriverV2/hypervisorDriver.c
@@ -197,9 +197,19 @@ hypervisor_loader(module_t mod, int what, void *arg)
     case MOD_LOAD:
         printf("[*] Loading hypervisor module.\n");
         error = vmm_init();
+        char buffer[20];
+
+        snprintf(buffer, sizeof(buffer), "%d", error);
+        printf("Error number: %s\n", buffer);
 
     case MOD_UNLOAD:
         printf("[*] Unloading hypervisor module.\n");
+
+        char buffer[20];
+
+        snprintf(buffer, sizeof(buffer), "%d", error);
+        printf("Error number: %s\n", buffer);
+
  //       free(vmcb, M_DEVBUF);
  //       free(hsave, M_DEVBUF);
         break;
